0adf1833f40dff9ccede32216662414627e4af1a
Paulos Girmacew <paulosgirmachew28@gmail.com>
2026-03-21 15:22:17 +0300

    Finished the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,23 @@
 import React from "react";
 import Entry from "./Entry";
+import emojipedia from "./emojipedia";
+
+function createCard(emojipedia) {
+  return (
+    <Entry
+      emoji={emojipedia.emoji}
+      name={emojipedia.name}
+      meaning={emojipedia.meaning}
+    />
+  );
+}
 function App(props) {
-  return <Entry />;
+  return (
+    <div>
+      <h1 className="heading">Emojipedia</h1>
+
+      <dl className="dictionary"> {emojipedia.map(createCard)}</dl>
+    </div>
+  );
 }
 export default App;
--- src/Entry.jsx
+++ src/Entry.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 function Entry(props) {
   return (
-    <div>
-      <h1 className="heading">Emojipedia</h1>
-      <dl className="dictionary-container">
-        <div className="term">
-          {props.emojiTerm}
-          <dd>
-            <dt className="description">{props.description}</dt>
-          </dd>
-        </div>
-      </dl>
+    <div className="container">
+      <div className="term">
+        <dt className="emoji">
+          <span>{props.emoji}</span>
+          <br></br>
+          <span>{props.name}</span>
+        </dt>
+        <dd className="description">{props.meaning}</dd>
+      </div>
     </div>
   );
 }
