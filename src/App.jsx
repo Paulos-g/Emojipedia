7f50a561330fd8338fcdb9d20da0d321427b7018
Paulos Girmacew <paulosgirmachew28@gmail.com>
2026-03-22 06:10:57 +0300

    Used fat arrow functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,22 @@ import React from "react";
 import Entry from "./Entry";
 import emojipedia from "./emojipedia";
 
-function createCard(emojipedia) {
-  return (
-    <Entry
-      emoji={emojipedia.emoji}
-      name={emojipedia.name}
-      meaning={emojipedia.meaning}
-    />
-  );
-}
 function App(props) {
   return (
     <div>
       <h1 className="heading">Emojipedia</h1>
 
-      <dl className="dictionary"> {emojipedia.map(createCard)}</dl>
+      <dl className="dictionary">
+        {" "}
+        {emojipedia.map((emojipedia) => (
+          <Entry
+            emoji={emojipedia.emoji}
+            name={emojipedia.name}
+            meaning={emojipedia.meaning}
+          />
+        ))}
+        ;
+      </dl>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import emojipedia from "./emojipedia";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
+
+const meaning = emojipedia.map(function (emojiEntry) {
+  return emojiEntry.meaning.substring(0, 100);
+});
+console.log(meaning);
